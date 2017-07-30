fc3d7504637d1f313ab5767f20031b15b2bea148
Jennifer Ali <jenny_p_c@hotmail.com>
2017-07-30 23:02:08 +0100

    Style updates

---
+++ template-parts/page/content-skills.php
@@ -0,0 +1,113 @@
+<?php
+
+/*
+        SKILLS SECTION
+*/
+//# CUSTOM FIELDS
+
+//====================================================================================//
+?>
+
+<section class="skills-section">
+
+    <div class="container">
+
+        <!-- SKILL LISTS
+        =======================================================================-->
+
+        <div class="row bio-excerpt-section__skills">
+
+            <div class="col-sm-4">
+
+                <h2>Skills</h2>
+                    
+            </div><!-- .col -->
+
+            <div class="col-sm-4">
+
+                <ul class="list-unstyled skill-list">
+
+                    <li>
+                        <div class="skill-bar">
+                            <div class="skills css">HTML</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="skill-bar">
+                            <div class="skills css">CSS/SASS</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="skill-bar">
+                            <div class="skills css">jQuery</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="skill-bar">
+                            <div class="skills css">Javascript</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="skill-bar">
+                            <div class="skills css">Bootstrap</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="skill-bar">
+                            <div class="skills css">WordPress</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="skill-bar">
+                            <div class="skills css">PHP</div>
+                        </div>
+                    </li>
+
+                </ul><!-- .skill-list -->
+                    
+            </div><!-- .col -->
+
+            <div class="col-sm-4">
+
+                <ul class="list-unstyled skill-list">
+
+                    <li>
+                        <div class="skill-bar">
+                            <div class="skills css">GitHub</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="skill-bar">
+                            <div class="skills css">Gulp</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="skill-bar">
+                            <div class="skills css">Webpack</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="skill-bar">
+                            <div class="skills css">Photoshop</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="skill-bar">
+                            <div class="skills css">Node.js / express.js</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="skill-bar">
+                            <div class="skills css">MongoDB</div>
+                        </div>
+                    </li>
+
+                </ul><!-- .skill-list -->
+                    
+            </div><!-- .col -->
+
+        </div><!-- .row -->
+
+    </div><!-- .container -->
+
+</section><!-- .skills-section -->
