37d3e5dcc1f17f52ba2e4e1f3fa3aa0563c132be
Jennifer Ali <jenny_p_c@hotmail.com>
2017-08-01 23:10:29 +0100

    update

--- functions.php
+++ functions.php
@@ -14,7 +14,6 @@ endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 
 // END ENQUEUE PARENT ACTION
-
 //------- 	INC CUSTOM WIDGETS 	--------------//
 
 include 'inc/widgets.php';
